241678abb643bf11203d055a231d7b9807a6610d
Vladislav Onatskyi <vladislav.onatskiy@freshcodeit.net>
2022-10-17 08:04:20 +0300

    folder structure changed

--- practica/TypeScript/baza.ts
+++
@@ -1,7 +0,0 @@
-let names: string = "Vlad"
-let age: number = 22
-
-function speak (name: string, live: number) {
-    console.log(`Зовут его ${name} и он живёт уже ${live} года`)
-}
-speak(names, age)
